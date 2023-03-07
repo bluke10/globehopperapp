63f7786075a606cbff7f6f34ff18a38be678cf6d
bluke10 <brandon.luke111@gmail.com>
2023-03-07 12:59:24 -0500

    adding db connectivity and creating tables along with inserting some data

--- app/conn.py
+++ app/conn.py
@@ -1,3 +1,11 @@
 #Connect to mySQL Database 
 #python -m pip install mysql-connector-python
 
+import mysql.connector
+
+myconn = mysql.connector.connect(
+    host="localhost",
+    user="ally",
+    password="allyfinancial123",
+    database="globehopperapp"
+)
